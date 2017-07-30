dd1cc92e88f5799c77efdefd29b429ad15a7f3a7
John Kercheval <jkercheval@godaddy.com>
2017-07-29 23:00:02 -0700

    Problem 4 enforced min check and added mod 11 check

--- Problem 4/problem4.py
+++ Problem 4/problem4.py
@@ -10,14 +10,16 @@ def is_palindromic(candidate: int):
     return True
 
 
-def get_max_palindromic(number_base) -> int:
-    max_palindromic = number_base  # initial condition
-    tuple_1 = number_base
-    tuple_2 = number_base
-    min_tuple_2 = 1
-    while tuple_1 > min_tuple_2:
+def get_max_palindromic(number_min, number_max) -> int:
+    max_palindromic = number_max  # initial condition
+    tuple_1 = number_max
+    tuple_2 = number_max
+    min_tuple_1 = number_min
+    min_tuple_2 = number_min
+    while tuple_1 > min_tuple_2 and tuple_1 > min_tuple_1:
         test_product = tuple_1 * tuple_2
-        if is_palindromic(test_product):
+        # Mod 11 checks added based on analysis described in solution discussion and reduced runtime by 65%
+        if (tuple_1 % 11 == 0 or tuple_2 % 11 == 0) and is_palindromic(test_product):
             print(str(test_product) + " using " + str(tuple_1) + "*" + str(tuple_2))
             if (test_product > max_palindromic):
                 max_palindromic = test_product
@@ -32,13 +34,13 @@ def get_max_palindromic(number_base) -> int:
     return max_palindromic
 
 
-NUMBER_BASE = 999
+NUMBER_MAX = 999
+NUMBER_MIN = 100
 
-# 90909 using 999*91
 # 580085 using 995*583
 # 906609 using 993*913
 # 886688 using 968*916
 # 888888 using 962*924
 # 861168 using 932*924
 # Max palindromic for 999 is 906609
-print("Max palindromic for " + str(NUMBER_BASE) + " is " + str(get_max_palindromic(NUMBER_BASE)))
+print("Max palindromic for " + str(NUMBER_MAX) + " is " + str(get_max_palindromic(NUMBER_MIN, NUMBER_MAX)))
